5fd2e4ce05143c9711da5a489e6aa71a822ea5bc
RomaLetodiani <roman.letodiani03@gmail.com>
2023-12-29 23:46:16 +0400

    finish

---
+++ script.js
@@ -0,0 +1,8 @@
+const burger = document.querySelector('.burger')
+const nav = document.querySelector('.mobile-nav')
+
+function triggerMenu(){
+    burger.classList.toggle('active')
+    nav.classList.toggle('active')
+}
+console.log(burger)
